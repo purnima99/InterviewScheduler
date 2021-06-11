6acb14a663e847f65155954abcde4f65b0dec134
purnima99 <purnimalal009@gmail.com>
2021-06-12 05:27:47 +0530

    User Login setup done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :interview_schedules
-  root 'interview_schedules#index'
+  root 'interview_schedules#welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
