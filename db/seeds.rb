8fa0708adc9f087e74e1c747fd8455b4bffdf97a
purnima99 <purnimalal009@gmail.com>
2021-06-12 16:45:11 +0530

    Completed Version 1.5

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
  
-# Interviewer.create([
-#     {name: "Rose Jain", email_id: "rosejain123@gmail.com ", phone:"9845684768"},
-#     {name: "Aayushi Gupta", email_id: "aayushi.gupta@gmail.com", phone:"7895674432"},
-#     {name: "Ishangi Jain", email_id: "jainishangi@gmail.com", phone:"9823667840"}
-#   ])
+Interviewer.create([
+    {name: "Rose Jain", email_id: "rosejain123@gmail.com ", phone:"9845684768"},
+    {name: "Aayushi Gupta", email_id: "aayushi.gupta@gmail.com", phone:"7895674432"},
+    {name: "Ishangi Jain", email_id: "jainishangi@gmail.com", phone:"9823667840"}
+  ])
   
-#   Interviewee.create([
-#     {name: "Akshita Thakur", email_id: "akshita0707@gmail.com", phone:" 7785634400"},
-#     {name: "Purnima Lal", email_id: "purnimalal@gmail.com", phone:"9456023487"},
-#     {name: "Shimantika Das", email_id: "shimantika08das@gmail.com", phone:"7003758970"}
-#   ])
+  Interviewee.create([
+    {name: "Akshita Thakur", email_id: "akshita0707@gmail.com", phone:" 7785634400"},
+    {name: "Purnima Lal", email_id: "purnimalal@gmail.com", phone:"9456023487"},
+    {name: "Shimantika Das", email_id: "shimantika08das@gmail.com", phone:"7003758970"}
+  ])
 
-InterviewSchedule.create([
-    {interviewer_id: "2", interviewee_id: "2", start_time:"2021-06-12 13:20:00", end_time:"2021-06-12 14:20:00", resume:"Resume_PurnimaLal.pdf"}
-])
+#InterviewSchedule.create([
+#   {interviewer_id: "2", interviewee_id: "2", start_time:"2021-06-12 13:20:00", end_time:"2021-06-12 14:20:00", resume:"Resume_PurnimaLal.pdf"}
+#])
