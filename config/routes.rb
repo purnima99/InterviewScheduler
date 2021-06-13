91ceb453c3edefd827cf341ab6b6346ae4a8099f
purnima99 <purnimalal009@gmail.com>
2021-06-13 17:16:28 +0530

    interview question (url ques) done

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :interview_schedules
   root 'interview_schedules#welcome'
   get '/uploads/:filename', to: redirect('/uploads/%{filename}', status: 302)
+  get 'interview_schedule/:id/:email', to: 'interview_schedules#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
